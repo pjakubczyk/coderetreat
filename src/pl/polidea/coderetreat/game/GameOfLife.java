0b4a1871ac52c635164b34c4daca7a2ca1872c28
Przemysław Jakubczyk <przemyslaw.jakubczyk@polidea.pl>
2013-01-10 12:25:05 +0100

    Checking number of neighbours and processes next game steps

--- src/pl/polidea/coderetreat/game/GameOfLife.java
+++ src/pl/polidea/coderetreat/game/GameOfLife.java
@@ -13,7 +13,7 @@ public class GameOfLife {
     }
 
     public GameOfLife(TYPE type) {
-        this(100);
+        this(20);
         if (type == TYPE.LOAF) {
             matrix[1][2] = true;
             matrix[1][3] = true;
@@ -25,20 +25,20 @@ public class GameOfLife {
             matrix[3][4] = true;
 
             matrix[4][3] = true;
-        }  else if (type == TYPE.LWSS){
-            matrix[1][2] = true;
-            matrix[1][5] = true;
+        } else if (type == TYPE.LWSS) {
+            matrix[1][1] = true;
+            matrix[1][4] = true;
 
-            matrix[2][1] = true;
+            matrix[2][5] = true;
+
             matrix[3][1] = true;
             matrix[3][5] = true;
-            matrix[4][1] = true;
+
             matrix[4][2] = true;
             matrix[4][3] = true;
             matrix[4][4] = true;
-
+            matrix[4][5] = true;
         }
-
     }
 
     public int size() {
@@ -63,6 +63,44 @@ public class GameOfLife {
         return matrix[i + 1][j + 1];
     }
 
+    public void proceed() {
+        boolean[][] tempMatrix;
+        tempMatrix = new boolean[realSide][];
+        for (int i = 0; i < realSide; ++i) {
+            tempMatrix[i] = new boolean[realSide];
+        }
+
+        for (int i = 1; i < realSide - 1; ++i) {
+            for (int j = 1; j < realSide - 1; ++j) {
+
+                if (matrix[i][j]) {
+                    // alive
+                    tempMatrix[i][j] = countNeighbours(i, j) == 3 || countNeighbours(i, j) == 2;
+                } else {
+                    // dead
+                    tempMatrix[i][j] = countNeighbours(i, j) == 3;
+                }
+
+            }
+        }
+        matrix = tempMatrix;
+    }
+
+    int countNeighbours(int i, int j) {
+        int count = 0;
+        count += matrix[i - 1][j - 1] ? 1 : 0;
+        count += matrix[i - 1][j + 0] ? 1 : 0;
+        count += matrix[i - 1][j + 1] ? 1 : 0;
+
+        count += matrix[i + 0][j - 1] ? 1 : 0;
+        count += matrix[i + 0][j + 1] ? 1 : 0;
+
+        count += matrix[i + 1][j - 1] ? 1 : 0;
+        count += matrix[i + 1][j + 0] ? 1 : 0;
+        count += matrix[i + 1][j + 1] ? 1 : 0;
+        return count;
+    }
+
     public enum TYPE {
         LOAF,
         BLOCK,
@@ -70,4 +108,16 @@ public class GameOfLife {
         LWSS
     }
 
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
+                sb.append(matrix[i][j] ? 'X' : 'O');
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();    //To change body of overridden methods use File | Settings | File Templates.
+    }
 }
--- test/robospock/src/test/groovy/pl/polidea/coderetreat/game/GameOfLifeSpecification.groovy
+++ test/robospock/src/test/groovy/pl/polidea/coderetreat/game/GameOfLifeSpecification.groovy
@@ -1,6 +1,7 @@
 package pl.polidea.coderetreat.game
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class GameOfLifeSpecification extends Specification {
 
@@ -47,7 +48,7 @@ class GameOfLifeSpecification extends Specification {
         3 | 3 | false
     }
 
-    def "test initial LWSS game"(){
+    def "test initial LWSS game"() {
         when:
         def game = new GameOfLife(GameOfLife.TYPE.LWSS)
 
@@ -56,25 +57,82 @@ class GameOfLifeSpecification extends Specification {
 
         where:
         i | j | value
-        0 | 0 | false
-        0 | 1 | true
+        0 | 0 | true
+        0 | 1 | false
         0 | 2 | false
-        0 | 3 | false
-        0 | 4 | true
-        1 | 0 | true
+        0 | 3 | true
+        0 | 4 | false
+        1 | 0 | false
         1 | 1 | false
         1 | 2 | false
         1 | 3 | false
-        1 | 4 | false
+        1 | 4 | true
         2 | 0 | true
         2 | 1 | false
         2 | 2 | false
         2 | 3 | false
         2 | 4 | true
-        3 | 0 | true
+        3 | 0 | false
         3 | 1 | true
         3 | 2 | true
         3 | 3 | true
-        3 | 4 | false
+        3 | 4 | true
     }
+
+    def "check neighbours number #i #j"() {
+        when:
+        def game = new GameOfLife(GameOfLife.TYPE.LWSS)
+
+        then:
+        game.countNeighbours(i, j) == value
+        where:
+        i | j | value
+        1 | 1 | 0
+        2 | 1 | 2
+        4 | 3 | 2
+        3 | 4 | 5
+    }
+
+    @Unroll
+    def "check first step of LWSS game i:#i j:#j "() {
+        given:
+        def game = new GameOfLife(GameOfLife.TYPE.LWSS)
+
+        when:
+        game.proceed()
+
+        then:
+        game.get(i, j) == value
+
+        where:
+        i | j | value
+        0 | 0 | false
+        1 | 3 | true
+        2 | 3 | false
+        2 | 4 | true
+        2 | 5 | true
+    }
+
+    @Unroll
+    def "check second step off LWSS game i:#i j:#j "() {
+        given:
+        def game = new GameOfLife(GameOfLife.TYPE.LWSS)
+
+        when:
+        game.proceed()
+        game.proceed()
+
+        then:
+        game.get(i, j) == value
+
+        where:
+        i | j | value
+        1 | 2 | true
+        1 | 3 | true
+        1 | 4 | true
+        2 | 2 | false
+        4 | 1 | true
+    }
+
 }
+
